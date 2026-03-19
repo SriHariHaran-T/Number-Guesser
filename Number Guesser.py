b8d9103ca99b108e52142bc140b79a7bc79cafaf
SriHariHaran-T <tsrihariharan222009@gmail.com>
2026-03-19 14:31:19 +0530

    Made the program more organized

--- Number Guesser.py
+++ Number Guesser.py
@@ -4,6 +4,6 @@ guess = int(input("Guess a number between 1 and 10: "))
 if guess== number:
     print("Congratulations! You guessed the number.")
 elif guess < number:
-    print("Too low! Try again.")
+    print("Too low! Try again, but the number is higher: ", number)
 else:
-    print("Sorry, the number was", number)
+    print("Sorry, the number was:", number)
